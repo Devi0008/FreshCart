f89dca019da89d0c11e8016ace5f5b0d7396022c
= <=>
2026-05-08 23:51:30 +0530

    new bakend url update

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@
 // Production:          Set VITE_API_BASE in .env file
 // =============================================
 
-const API_BASE = import.meta.env.VITE_API_BASE || 'https://durgapalstore.shop';
+const API_BASE = import.meta.env.VITE_API_BASE || 'http://162.55.25.67/~durgapal/';
 
 if (import.meta.env.DEV) {
     console.info('[FreshCart] API_BASE =', API_BASE);
